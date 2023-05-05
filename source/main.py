da05b2a7f703b34333a930a79db2d9de221ca408
Matheus_Barreto <matheusbarreto.dev@gmail.com>
2023-05-05 00:26:15 -0300

    Update main.py

--- source/main.py
+++ source/main.py
@@ -1,31 +1,36 @@
-import pytesseract
-import cv2
-import re
 import os
+import re
 import fnmatch
+import cv2
+import pytesseract
+from pdf2image import convert_from_path
 
-caminho_imagens = r"C:\Users\Rose\Documents\Gteste"
+caminho_imagens = r"C:\Users\Matheus\Documents\PYTHON\Leitor_de_PDF"
+caminho_tesseract = r"C:\Program Files\Tesseract-OCR"
+pytesseract.pytesseract.tesseract_cmd = caminho_tesseract + r"\tesseract.exe"
 
-with open('texto.txt', 'w', encoding='utf-8') as f:
-    avisoLocal = "Os cpfs encontrados foram: "
+for nome_arquivo in os.listdir(caminho_imagens):
+    if fnmatch.fnmatch(nome_arquivo, '*.pdf'):
+        caminho_arquivo = os.path.join(caminho_imagens, nome_arquivo)
 
-    for nome_arquivo in os.listdir(caminho_imagens):
-        if fnmatch.fnmatch(nome_arquivo, '*.jpg'):
-            caminho_arquivo = os.path.join(caminho_imagens, nome_arquivo)
+        imagens = convert_from_path(caminho_arquivo)
+        for imagem in imagens:
+            temp_img_path = 'temp_img.jpg'
+            imagem.save(temp_img_path, 'JPEG')
 
-            imagem = cv2.imread(caminho_arquivo)
-            caminho = r"C:\Program Files\Tesseract-OCR"
-            pytesseract.pytesseract.tesseract_cmd = caminho + r"\tesseract.exe"
-            texto = pytesseract.image_to_string(imagem)
+            texto = pytesseract.image_to_string(cv2.imread(temp_img_path))
+            print(texto)
 
-            if not texto:
-                print(f"Não foi possível identificar o texto na imagem {caminho_arquivo}.")
-            else:
-                cpfs = re.findall(r'\d{3}\.\d{3}\ .\d{3}-\d{2}', texto)
-                cpfs_numeros = []
-                for cpf in cpfs:
-                    cpf_numeros = re.sub(r'\D', '', cpf)
-                    cpfs_numeros.append(cpf_numeros)
-                print(f"CPF(s) na imagem {caminho_arquivo} é: {cpfs_numeros}")
-                str_cpfs = ",".join(cpfs_numeros)
-                f.write(avisoLocal + str_cpfs + "\n")
+            os.remove(temp_img_path)
+
+        if not texto:
+            print(f"Não foi possível identificar o texto na imagem {caminho_arquivo}.")
+        else:
+            cpfs = re.findall(r'\d{3}\.\d{3}\ .\d{3}-\d{2}', texto)
+            cpfs_numeros = []
+            for cpf in cpfs:
+                cpf_numeros = re.sub(r'\D', '', cpf)
+                cpfs_numeros.append(cpf_numeros)
+            print(f"CPF(s) na imagem {caminho_arquivo} é: {cpfs_numeros}")
+            #str_cpfs = ",".join(cpfs_numeros)
+            #f.write(avisoLocal + str_cpfs + "\n")
