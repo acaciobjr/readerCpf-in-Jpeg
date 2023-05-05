cb14a6fedcbdca1775910a651bef5992342a7f1b
Matheus_Barreto <matheusbarreto.dev@gmail.com>
2023-05-05 10:45:05 -0300

    Correções e Teste
    Alteração do caminho da IMG
    Correção do espaço que tinha no findall
    Adição de PDFs para teste

--- source/main.py
+++ source/main.py
@@ -5,7 +5,7 @@ import cv2
 import pytesseract
 from pdf2image import convert_from_path
 
-caminho_imagens = r"C:\Users\Matheus\Documents\PYTHON\Leitor_de_PDF"
+caminho_imagens = r"C:\Users\Matheus\Documents\GitHub\Leitor_de_Arquivo\files"
 caminho_tesseract = r"C:\Program Files\Tesseract-OCR"
 pytesseract.pytesseract.tesseract_cmd = caminho_tesseract + r"\tesseract.exe"
 
@@ -14,23 +14,23 @@ for nome_arquivo in os.listdir(caminho_imagens):
         caminho_arquivo = os.path.join(caminho_imagens, nome_arquivo)
 
         imagens = convert_from_path(caminho_arquivo)
+        #Convertendo PDF em JPEG para a leitura do arquivo
         for imagem in imagens:
             temp_img_path = 'temp_img.jpg'
             imagem.save(temp_img_path, 'JPEG')
-
+            #Armazenando o texto do JPEG
             texto = pytesseract.image_to_string(cv2.imread(temp_img_path))
-            print(texto)
-
+            #Apagando arquivo
             os.remove(temp_img_path)
 
-        if not texto:
-            print(f"Não foi possível identificar o texto na imagem {caminho_arquivo}.")
-        else:
-            cpfs = re.findall(r'\d{3}\.\d{3}\ .\d{3}-\d{2}', texto)
-            cpfs_numeros = []
-            for cpf in cpfs:
-                cpf_numeros = re.sub(r'\D', '', cpf)
-                cpfs_numeros.append(cpf_numeros)
-            print(f"CPF(s) na imagem {caminho_arquivo} é: {cpfs_numeros}")
-            str_cpfs = ",".join(cpfs_numeros)
-            f.write(avisoLocal + str_cpfs + "\n")
+            if not texto:
+                print(f"Não foi possível identificar o texto na imagem {caminho_arquivo}.")
+            else:
+                cpfs = re.findall(r'\d{3}\.\d{3}\.\d{3}-\d{2}', texto)
+                cpfs_numeros = []
+                for cpf in cpfs:
+                    cpf_numeros = re.sub(r'\D', '', cpf)
+                    cpfs_numeros.append(cpf_numeros)
+                print(f"CPF(s) na imagem {caminho_arquivo} é: {cpfs_numeros}")
+                #str_cpfs = ",".join(cpfs_numeros)
+                #f.write(avisoLocal + str_cpfs + "\n")
