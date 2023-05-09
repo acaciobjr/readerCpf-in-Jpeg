0b30245afde6185400e04da5a512697f8db35bcd
Acácio <99909819+acaciobjr@users.noreply.github.com>
2023-05-09 11:59:47 -0300

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,31 +1,53 @@
 import pytesseract
 import cv2
-import re
 import os
-import fnmatch
+import re
+import cv2
+import pytesseract
+from pdf2image import convert_from_path
 
+# caminhos
 caminho_imagens = r"C:\Users\Rose\Documents\Gteste"
+caminho_tesseract = r"C:\Program Files\Tesseract-OCR"
+pytesseract.pytesseract.tesseract_cmd = os.path.join(caminho_tesseract, "tesseract.exe")
+
+
+def processar_texto(texto, caminho_arquivo, f):
+    if not texto:
+        print(f"Não foi possível identificar o texto no arquivo {caminho_arquivo}.")
+        return
+
+    cpfs = re.findall(r'\d{3}\.\d{3}\.\d{3}-\d{2}', texto)
+    RG = re.findall(r'\d{13}', texto)
+    print(RG)
+    cpfs_numeros = [re.sub(r'\D', '', cpf) for cpf in cpfs]
+
+    if cpfs_numeros:
+        avisoLocal = f"CPF e RG no arquivo {caminho_arquivo} são: {cpfs_numeros} e {RG}"
+        print(avisoLocal)
+        str_cpfs = ",".join(cpfs_numeros)
+        f.write(avisoLocal + "\n")
+    else:
+        print(f"Não foi possível identificar CPFs no arquivo {caminho_arquivo}.")
+
 
 with open('texto.txt', 'w', encoding='utf-8') as f:
-    avisoLocal = "Os cpfs encontrados foram: "
-
     for nome_arquivo in os.listdir(caminho_imagens):
-        if fnmatch.fnmatch(nome_arquivo, '*.jpg'):
-            caminho_arquivo = os.path.join(caminho_imagens, nome_arquivo)
+        extensao = os.path.splitext(nome_arquivo)[1].lower()
+        caminho_arquivo = os.path.join(caminho_imagens, nome_arquivo)
+        texto = ''
 
+        if extensao == '.pdf':
+            imagens = convert_from_path(caminho_arquivo)
+            for imagem in imagens:
+                temp_img_path = 'temp_img.jpg'
+                imagem.save(temp_img_path, 'JPEG')
+                texto += pytesseract.image_to_string(cv2.imread(temp_img_path))
+                os.remove(temp_img_path)
+
+        elif extensao == '.jpg':
             imagem = cv2.imread(caminho_arquivo)
-            caminho = r"C:\Program Files\Tesseract-OCR"
-            pytesseract.pytesseract.tesseract_cmd = caminho + r"\tesseract.exe"
             texto = pytesseract.image_to_string(imagem)
 
-            if not texto:
-                print(f"Não foi possível identificar o texto na imagem {caminho_arquivo}.")
-            else:
-                cpfs = re.findall(r'\d{3}\.\d{3}\ .\d{3}-\d{2}', texto)
-                cpfs_numeros = []
-                for cpf in cpfs:
-                    cpf_numeros = re.sub(r'\D', '', cpf)
-                    cpfs_numeros.append(cpf_numeros)
-                print(f"CPF(s) na imagem {caminho_arquivo} é: {cpfs_numeros}")
-                str_cpfs = ",".join(cpfs_numeros)
-                f.write(avisoLocal + str_cpfs + "\n")
+        if texto:
+            processar_texto(texto, caminho_arquivo, f)
