7ad3e1e45d2ed54b21e7ff0b26db4b2b2be7af1e
Acácio <99909819+acaciobjr@users.noreply.github.com>
2023-06-01 13:12:20 -0300

    Update Doc_Reader.py

--- Doc_Reader.py
+++ Doc_Reader.py
@@ -5,36 +5,40 @@ import pytesseract
 from pdf2image import convert_from_path
 
 # caminhos
-caminho_imagens = r"diretório onde estão as documentações em arquivos pdf e jpg"
+caminho_imagens = r"C:\Users\Rose\Documents\Gteste"
 caminho_tesseract = r"C:\Program Files\Tesseract-OCR"
 pytesseract.pytesseract.tesseract_cmd = os.path.join(caminho_tesseract, "tesseract.exe")
 
 
 def processar_texto(texto, caminho_arquivo, f):
-    if not texto:
-        print(f"Não foi possível identificar o texto no arquivo {caminho_arquivo}.")
-        return
-
+    #if not texto:
+     #   print(f"Não foi possível identificar o texto no arquivo {caminho_arquivo}.")
+      #  return
+    texto = texto.replace(" ", "")
+    nasc = re.findall(r'\d{2}/\d{2}/\d{4}', texto)
     cpfs = re.findall(r'\d{3}\.\d{3}\.\d{3}-\d{2}', texto)
     RG = re.findall(r'\d{13}', texto)
     #print(RG)
     cpfs_numeros = [re.sub(r'\D', '', cpf) for cpf in cpfs]
 
     if cpfs_numeros or RG:
-        avisoLocal = f"CPF e RG no arquivo {caminho_arquivo} são: {cpfs_numeros} e {RG}"
+        avisoLocal = f"CPF e RG no arquivo {caminho_arquivo} são: {cpfs_numeros} e {RG}. nascimento:{nasc}"
+        #print(texto)
         print(avisoLocal)
         str_cpfs = ",".join(cpfs_numeros)
         f.write(avisoLocal + "\n")
-    else:
-        print(f"Não foi possível identificar CPFs no arquivo {caminho_arquivo}.")
 
+    else:
+        avisoFalha = f"Não foi possível identificar CPF e RG no arquivo {caminho_arquivo}."
+        print(avisoFalha)
+        f.write(avisoFalha + "\n")
 
 with open('texto.txt', 'w', encoding='utf-8') as f:
     for nome_arquivo in os.listdir(caminho_imagens):
         extensao = os.path.splitext(nome_arquivo)[1].lower()
         caminho_arquivo = os.path.join(caminho_imagens, nome_arquivo)
         texto = ''
-
+        #if
         if extensao == '.pdf':
             imagens = convert_from_path(caminho_arquivo)
             for imagem in imagens:
